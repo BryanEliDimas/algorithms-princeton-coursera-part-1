bb35f6d9389e82d475f6cc60c28357f6d908195f
bryanelidimas <bryanelidimas@gmail.com>
2016-03-28 17:43:09 -0500

    Create our starting point for  Weighted Quick Union with Path Compression UF (WeightedQuickUnion renamed)

---
+++ Union Find/WeightedQuickUnionPCUF.java
@@ -0,0 +1,59 @@
+public class WeightedQuickUnionPCUF {
+  private int[] id;
+  private int count;
+  private int[] sz;
+
+  public WeightedQuickUnionPCUF(int N){
+    count = N;
+    id = new int[N];
+
+    for(int i = 0; i < N; i++) id[i] = i;
+
+    sz = new int[N];
+
+    for(int i = 0; i < N; i++) sz[i] = 1;
+  }
+
+  public int count(){
+    return count;
+  }
+
+  public boolean connected(int p, int q){
+    return find(p) == find(q);
+  }
+
+  public int find(int p){
+    while(p != id[p]) p = id[p];
+
+    return p;
+  }
+
+  public void union(int p, int q){
+    int i = find(p);
+    int j = find(q);
+
+    if(i == j) return;
+
+    if(sz[i] < sz[j]) { id[i] = j; sz[j] += sz[i]; }
+    else { id[j] = i; sz[i] += sz[j]; }
+
+    count--;
+  }
+
+  public static void main(String[] args) {
+    int N = StdIn.readInt();
+    WeightedQuickUnionPCUF uf = new WeightedQuickUnionPCUF(N);
+
+    while(!StdIn.isEmpty()){
+      int p = StdIn.readInt();
+      int q = StdIn.readInt();
+
+      if(uf.connected(p, q)) continue;
+      uf.union(p, q);
+
+      StdOut.println(p + " " + q);
+    }
+
+    StdOut.println(uf.count() + " components");
+  }
+}
